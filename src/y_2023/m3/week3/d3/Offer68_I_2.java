eb150633c86fe1aff50f09c1354a3b2636c77ec7
yangshihao-arron <2867351265@qq.com>
2023-03-23 21:27:03 +0800

    add some solutions about Offer

---
+++ src/y_2023/m3/week3/d3/Offer68_I_2.java
@@ -0,0 +1,23 @@
+package y_2023.m3.week3.d3;
+
+public class Offer68_I_2 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right, p, q);
+         else if(root.val > p.val &&root.val > q.val)
+             return lowestCommonAncestor(root.left, p, q);
+         else
+             return root;
+    }
+}
---
+++ src/y_2023/m3/week3/d4/Offer07_2.java
@@ -0,0 +1,21 @@
+package y_2023.m3.week3.d4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Offer07_2 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+
+}
