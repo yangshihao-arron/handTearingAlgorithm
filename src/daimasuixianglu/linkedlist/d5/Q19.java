95694be3bb5704b3d4795136733e00aa5a10152b
yangshihao-arron <2867351265@qq.com>
2023-05-13 12:04:14 +0800

    add daily practice(2023/5/13)

---
+++ src/daimasuixianglu/linkedlist/d5/Q19.java
@@ -0,0 +1,25 @@
+package daimasuixianglu.linkedlist.d5;
+
+public class Q19 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return null;
+    }
+}
